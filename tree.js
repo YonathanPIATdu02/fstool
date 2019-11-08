01a014f20715d82a0e38ece8b564373f035ab1e2
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2019-11-08 15:18:20 +0100

    tree.js terminé

--- tree.js
+++ tree.js
@@ -1,11 +1,24 @@
+const fs = require('fs');
 
-function printFilename( filename, isDirectory, indent){
+function printFilename(filename, isDirectory, indent) {
     console.log(indent + (isDirectory ? '+ ' : '  ') + filename);
 }
 
 function printDirFiles(dirname, indent) {
-    readdirSync(dirname)
+    fs.readdirSync(dirname, { withFileTypes: true }).forEach(element => {
+        printFilename(element.name, element.isDirectory(), indent);
+        if (element.isDirectory()) {
+            printDirFiles(dirname + element.name + "/", indent + "  ")
+        }
+    });
 }
 
+function start(startFilename) {
+    var statSync = fs.lstatSync(startFilename).isDirectory();
+    printFilename(startFilename, statSync, "");
+    if (statSync) {
+        printDirFiles(startFilename, "  ");
+    }
+}
 
-printFilename('bonjour', false, '    ');
+start(process.argv[2]);
