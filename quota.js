fca041c0f0f0a42acb5f50b14b75b0531b228b01
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2019-11-12 17:30:19 +0100

    erreur dans lstat

--- quota.js
+++ quota.js
@@ -1,1 +1,31 @@
-console.log("ok boomer");
+const fs = require('fs').promises;
+
+async function computeSize(dirname, dirent) {
+    if (!dirent.isDirectory()) {
+        return fs.lstat(dirname + "/" + dirent)
+            .then((err, stats) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    quota += stats.size;
+                }
+            });
+    } else {
+        return computeDirectorySize(dirname + "/" + dirent);
+    }
+}
+
+async function computeDirectorySize(dirname) {
+    return fs.readdir(dirname, { withFileTypes: true })
+        .then(dirents => {
+            return Promise.all(dirents.map(dirent => computeSize(dirname, dirent)));
+        })
+}
+
+async function start(startDirname) {
+    let quota = 0;
+    computeDirectorySize(startDirname);
+    return quota;
+}
+
+start(process.argv[2] || './');
