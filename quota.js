fef9a8d51413ecafb1419e8ee907d0237b845c61
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2019-11-14 12:17:06 +0100

    process.cpuUsage a ecrire la conversion

--- quota.js
+++ quota.js
@@ -1,17 +1,15 @@
 const fs = require('fs').promises;
+const startUsage = process.cpuUsage()
+var quota;
 
 async function computeSize(dirname, dirent) {
     if (!dirent.isDirectory()) {
-        return fs.lstat(dirname + "/" + dirent)
-            .then((err, stats) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    quota += stats.size;
-                }
+        return fs.lstat(dirname + "/" + dirent.name)
+            .then((stats) => {
+                quota += stats.size;
             });
     } else {
-        return computeDirectorySize(dirname + "/" + dirent);
+        return computeDirectorySize(dirname + "/" + dirent.name);
     }
 }
 
@@ -23,9 +21,20 @@ async function computeDirectorySize(dirname) {
 }
 
 async function start(startDirname) {
-    let quota = 0;
-    computeDirectorySize(startDirname);
-    return quota;
+    quota = 0;
+    computeDirectorySize(startDirname)
+        .then(() => console.log("Size : " + quota + " Bytes"))
+        .then(() => console.log("Unit Size : " + humanFileSize(quota) + " Bytes"))
+        .then(() => console.log(process.cpuUsage(startUsage).system))
+        .catch(err => console.log(err));
+}
+
+function humanFileSize(size) {
+    if (size === 0) return '0 Bytes';
+    const byteSizes = ['Bytes', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi', 'Yi'];
+    const i = Math.floor(Math.log(size) / Math.log(1024));
+    return parseFloat((size / Math.pow(1024, i)).toFixed(2)) + byteSizes[i];
 }
 
 start(process.argv[2] || './');
+
