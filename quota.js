62abc492162d7720fc5e22c2219c8f00adccba1f
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2019-11-15 15:20:25 +0100

    Paramétrage de la commande quota en cour

--- quota.js
+++ quota.js
@@ -1,5 +1,9 @@
+#!/usr/bin/env node
+
 const fs = require('fs').promises;
 const startUsage = process.cpuUsage()
+const _cliProgress = require('cli-progress');
+const bar = new _cliProgress.SingleBar({}, _cliProgress.Presets.shades_classic);
 var quota;
 
 async function computeSize(dirname, dirent) {
@@ -7,6 +11,7 @@ async function computeSize(dirname, dirent) {
         return fs.lstat(dirname + "/" + dirent.name)
             .then((stats) => {
                 quota += stats.size;
+                bar.increment(stats.size / 1048576);
             });
     } else {
         return computeDirectorySize(dirname + "/" + dirent.name);
@@ -22,19 +27,32 @@ async function computeDirectorySize(dirname) {
 
 async function start(startDirname) {
     quota = 0;
+    bar.start(5000,0);
     computeDirectorySize(startDirname)
-        .then(() => console.log("Size : " + quota + " Bytes"))
-        .then(() => console.log("Unit Size : " + humanFileSize(quota) + " Bytes"))
-        .then(() => console.log(process.cpuUsage(startUsage).system))
-        .catch(err => console.log(err));
-}
+        .then(() => {
+        bar.stop();
+        console.log("Size : " + quota + " Bytes");
+        console.log("Unit Size : " + humanFileSize(quota) + " Bytes");
+        console.log("CPU Usage : " + humanTimeSize(process.cpuUsage(startUsage).user / 1000));
+        });}
 
 function humanFileSize(size) {
     if (size === 0) return '0 Bytes';
-    const byteSizes = ['Bytes', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi', 'Yi'];
+    const byteSizes = ['', 'Ki', 'Mi', 'Gi'];
     const i = Math.floor(Math.log(size) / Math.log(1024));
     return parseFloat((size / Math.pow(1024, i)).toFixed(2)) + byteSizes[i];
 }
 
-start(process.argv[2] || './');
+function humanTimeSize(time){
+    if (time > 1000)
+    {
+        var humanTimeSize = time + " secondes";
+    }
+    else
+    {
+        var humanTimeSize = time + " millisecondes";
+    }
+    return humanTimeSize
+}
 
+start(process.argv[2] || process.env.HOME);
